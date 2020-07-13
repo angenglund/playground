184b6fda1e797b95d7d039a75545beddd4555a60
A. Englund <angelaenglund@gmail.com>
2020-07-13 16:44:58 -0700

    fff

--- lifepath.py
+++ lifepath.py
@@ -1,9 +1,9 @@
 # life path number assigns a number to the month you were born in and then adds the numbers from the rest
 # and reduces them down to a single digit (unless it's 11 or 22)
-# MAY = 5
+# MONTH may/5 = 5
 
 # DAY = 29
-# = 2 + 9 = 11 (no further reduction)
+# 2 + 9 = 11 (no further reduction)
 
 # YEAR = 1981
 # = 1 + 9 + 8 + 1 = 19
@@ -15,6 +15,57 @@
 
 # my life path number = 8
 
-import numpy
+import numpy as np
+from functools import reduce
+
+
+monthsum = []
+daysum = []
+yearsum = []
+prefinal = []
+
+
+# ask the user for their month, day, and year of birth
+month = input("Enter the number of the month of your birth \n")
+day = input("Enter the number for the day of your birth \n")
+year = input("Enter the full year of your birth \n")
+#print("So your full birth date is " + month + "/" + day + "/" + year + "?")
+
+
+# take user's input for month and reduce down to single digit unless it's 11 and print the result
+# WORKING
+if month == "11":
+    print("11 is correct here.")
+    # append this to prefinal
+else:
+    monthsum = sum(int(digit) for digit in str(month))
+    print("result of month is " + str(monthsum)
+    # append this to prefinal
+
+
+# reduce the user's input for the day of their birth down to a single digit unless it's 11 or 22 and print the result
+# WAS WORKING
+if day == "11" or day == "22":
+    print("result of day is " + day + ".")
+    # append this to prefinal
+else:
+    daysum = sum(int(digit) for digit in str(day))
+    if daysum == "11":
+        print("you got 11 for daysum.")
+    # append this to prefinal
+
+# reduce the user's input for the year of their birth down to a single digit unless it's 11 or 22 and print
+# first part WORKING
+yearsum = sum(int(digit) for digit in str(year))
+
+if str(yearsum) == "11" or str(yearsum) == "22": # this is working
+    print("Wow " + str(yearsum) + "," + " aren't you special.") # this is working
+else:
+    if str(yearsum) < "10":
+        print("year reduced a 2nd time")
+
+print(yearsum)
+
+
 
 
